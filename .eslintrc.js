5f816c9c9616b23fa711f36f86e1ce791e036f78
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2022-03-23 13:01:10 +0900

    Add ESLint + Airbnb Style

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.eslint.json', './tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'avoid',
+  printWidth: 80,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'es5',
+};
