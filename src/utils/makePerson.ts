f3b1ee0a05803c3cc2f3068815ad458f640d974c
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2022-03-31 21:37:54 +0900

    Log With Turbo Console Log

--- src/utils/makePerson.ts
+++ src/utils/makePerson.ts
@@ -1,2 +1,9 @@
 export const makePerson = (name: string, age: number) => ({ name, age });
-export const testMakePerson = () => console.log(makePerson('SynCROSS', 19));
+export const testMakePerson = () => {
+  const person = makePerson('SynCROSS', 19);
+  console.log(
+    `file: makePerson.ts ~ line 4 ~ testMakePerson ~ person ${JSON.parse(
+      JSON.stringify(person)
+    )}`
+  );
+};
